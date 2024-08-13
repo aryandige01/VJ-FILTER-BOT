c2b7fe169fb650c9eff1838eeba81e5766161454
aryandige01 <digevaishali@gmail.com>
2024-08-13 21:21:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_request_group_hd_movie')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AD_FILMS_STORE')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Aryan_file_bot?start=BQADAQADEgkAAuo0YESihDbsusD0eBYE')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+dHX-yoLufAwyY2M1') # Support Chat Link Without https:// or @
 
 # True Or False
