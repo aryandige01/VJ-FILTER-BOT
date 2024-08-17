5e1110a5350c12c3983136537e062748a81ea99d
aryandige01 <digevaishali@gmail.com>
2024-08-17 21:32:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kfcinemas:kfcinemasmovie@cluster0.m8diz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://moviesott:moviesotttt@moviesott.tbsmi.mongodb.net/?retryWrites=true&w=majority&appName=moviesott")
 DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'kfcinemas')
 
